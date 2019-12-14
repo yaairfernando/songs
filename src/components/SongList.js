8463d9295dd2ca8ce59807eef6749dacc809f04a
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-14 14:41:48 -0600

    Calling the action creators from the components

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import SongItem from './SongItem';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   
-  // renderList() {
-  //   return this.props.songs.map((song) => {
-  //     return (
-  //       <div className="item" key={song.title}>
-  //         <div className="right floated content">
-  //           <button className="ui button primary">
-  //             Select
-  //           </button>
-  //         </div>
-  //         <div className="content">
-  //           {song.title}
-  //         </div>
-  //       </div>
-  //     );
-  //   });
-  // }
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button 
+              className="ui button primary" 
+              onClick={() => this.props.selectSong(song)}  
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">
+            {song.title}
+          </div>
+        </div>
+      );
+    });
+  }
 
   
   render() {
-    const songs = this.props.songs.map((song) => {
-      return <SongItem key={song.title} song={song} />
-    })
     return (
       <div className="ui divided list">
-        { songs }  
+        { this.renderList() }  
       </div>
     );
   };
 };
 
 const mapStateToProps = state => {
+  console.log(state);
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
